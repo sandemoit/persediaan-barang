c64650326502a110824285731da0858de26f9fb9
Sandi Maulika <sandimaulidika@gmail.com>
2025-06-01 22:46:11 +0700

    refactor validasi auth

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -15,8 +15,14 @@ class Auth extends CI_Controller
         if ($this->session->userdata('email')) {
             redirect('dashboard');
         }
-        $this->form_validation->set_rules('email', 'Email', 'trim|required|valid_email');
-        $this->form_validation->set_rules('password', 'Password', 'trim|required');
+
+        $this->form_validation->set_rules('email', 'Email', 'trim|required|valid_email', [
+            'required' => 'Email harus diisi!',
+            'valid_email' => 'Email tidak valid!'
+        ]);
+        $this->form_validation->set_rules('password', 'Password', 'trim|required', [
+            'required' => 'Password harus diisi!'
+        ]);
 
         if ($this->form_validation->run() == false) {
             $data['title'] = 'Sistem Informasi Gudang';
